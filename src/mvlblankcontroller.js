d5eed09370bafc308550d283a37d94a6e6cb81b6
mvoevodskiy <mv@aaa0.ru>
2020-01-27 07:15:23 +0300

    Initiated with XLSX

--- src/mvlblankcontroller.js
+++ src/mvlblankcontroller.js
@@ -1,9 +1,30 @@
 const MVLoaderBase = require('mvloader/src/mvloaderbase');
+const XLSX = require('xlsx');
 
 class MVLExcelController extends MVLoaderBase {
 
     constructor (...config) {
-        super(...config);
+        let locaDefaults = {
+            sheetName: 'sheet',
+        };
+        super(locaDefaults, ...config);
+    }
+
+    async init() {
+        super.init();
+        this.Excel = XLSX;
+    }
+
+    writeFile (filename, data = []) {
+        let workbook = XLSX.utils.book_new();
+        if (Array.isArray(data)) {
+
+        }
+
+        let ws_name = this.config.sheetName;
+        let workSheet = XLSX.utils.aoa_to_sheet(data);
+        XLSX.utils.book_append_sheet(workbook, workSheet, ws_name);
+        return XLSX.writeFile(workbook, filename);
     }
 
 }
